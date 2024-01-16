542d2af81f6a4d1d6bcd186e7cb7bb2535eb0b92
Pooja Tikhe <tikhepooja11@gmail.com>
2024-01-17 00:08:05 +0530

    Hided my mongo atlas credentials by adding in .env file

--- models/db.js
+++ models/db.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
+const MONGO_USERNAME = process.env.MONGO_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 
 mongoose.connect(
-  "mongodb+srv://pooja:pooja123@cluster0.sygdhbi.mongodb.net/UserDB_Baxture",
+  `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.sygdhbi.mongodb.net/UserDB_Baxture`,
   (error) => {
     if (error) console.log(`Error in connection: ${error}`);
     else console.log(`Connected to DB`);
